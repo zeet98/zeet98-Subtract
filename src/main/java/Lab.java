113c93083169f5f2f2ad1c3bb54faae5ab5ae5f3
Zeshan Tawa <zeshan.tawa98@gmail.com>
2023-02-22 01:21:15 +0000

    Complete subtract assignment

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,7 @@ public class Lab {
      * @return a minus b
      */
     public double sub(double a, double b){
-        return 0;
+        double result = a - b;
+        return result;
     }
 }
